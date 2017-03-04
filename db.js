78829d0e089806da1d21614a40c67625c44d9961
jsu2 <jsu2@cisco.com>
2017-03-03 16:08:02 -0800

    update db

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var MongoClient = require('mongodb').MongoClient, assert = require('assert');
-var dbUrl = 'mongodb://localhost:27017/wave-checker';
+// var dbUrl = 'mongodb://localhost:27017/wave-checker';
+var dbUrl = 'mongodb://heroku_xl863h80:Gbse$1234@ds011890.mlab.com:11890/heroku_xl863h80';
 module.exports.DB = function(name, callback) {
     MongoClient.connect(dbUrl, function(err, db) {
         var collection = db.collection(name);
